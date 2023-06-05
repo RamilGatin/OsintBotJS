d4752345d53853d8ae6e552f61bff79602d4c74b
RAMIL <nais.gatin@yandex.ru>
2023-06-05 15:53:36 +0300

    base64

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
+const fs = require('fs');
+const https = require('https');
 
 // Задаем API ключ для телеграмм бота и API ключ для сайта search4faces.com
 const telegramBotToken = '6044787090:AAHp51UHwLBdAgg3erIgCuG0sJB-HaQgQdU';
@@ -15,79 +17,120 @@ bot.onText(/\/start/, (msg) => {
 });
 
 // Обработчик получения изображения от пользователя
+let base64Image = ""
 bot.on('photo', async (msg) => {
     const chatId = msg.chat.id;
     const fileId = msg.photo[msg.photo.length - 1].file_id;
 
+    bot.getFile(fileId).then((fileInfo) => {
+        const fileUrl = `https://api.telegram.org/file/bot${telegramBotToken}/${fileInfo.file_path}`;
+        const fileName = `photo_${Date.now()}.jpg`;
+        const imageStream = fs.createWriteStream(fileName);
+        // Скачиваем и сохраняем фотографию в файл
+        https.get(fileUrl, (response) => {
+            response.pipe(imageStream);
+            response.on('end', () => {
+                // Читаем содержимое файла, преобразуем его в base64 и удаляем исходный файл
+                fs.readFile(fileName, (err, data) => {
+                    if (err) throw err;
+                    base64Image = data.toString('base64');
+                    // fs.unlink(fileName, () => {
+                    //     console.log(`File ${fileName} was deleted`);
+                    // });
+                });
+            });
+        });
+    });
+
+
     // Получаем информацию о файле изображения
     const fileInfo = await bot.getFile(fileId);
     console.log("fileInfo", fileInfo)
-    // Скачиваем изображение с сервера телеграмм и отправляем его на сервер search4faces.com для обнаружения лиц
-    // const response = await axios.post('https://search4faces.com/api/json-rpc/v1', {
-    //     jsonrpc: '2.0',
-    //     method: 'detectFaces',
-    //     id: 'some-id',
-    //     params: {
-    //         image: fileInfo.fileLink
-    //     }
-    // }, {
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         'x-authorization-token': search4facesToken
-    //     }
-    // });
 
-    const response = await axios({
-            method: "post",
-            url: "https://search4faces.com/api/json-rpc/v1",
-            headers: {
-                'Content-Type': 'application/json',
-                'x-authorization-token': search4facesToken
-            },
-            data: {
-                jsonrpc: '2.0',
-                method: 'detectFaces',
-                id: 'some-id',
-                params: {
-                    image: fileInfo.fileLink
-                }
-            }
+    // Заглушка response
+    // Todo: Разобраться с телеграм ботом, чтобы он возвращал base64 кодировку
+    // const response = {
+    //     "jsonrpc": "2.0",
+    //     "result": {
+    //         "image": "6479f4e3aca844.75881736.jpg",
+    //         "faces": [
+    //             {
+    //                 "x": 366,
+    //                 "y": 359,
+    //                 "width": 389,
+    //                 "height": 525,
+    //                 "lm1_x": 458,
+    //                 "lm1_y": 566,
+    //                 "lm2_x": 630,
+    //                 "lm2_y": 572,
+    //                 "lm3_x": 506,
+    //                 "lm3_y": 679,
+    //                 "lm4_x": 442,
+    //                 "lm4_y": 747,
+    //                 "lm5_x": 616,
+    //                 "lm5_y": 765
+    //             }
+    //         ]
+    //     },
+    //     id: 100
+    // }
+    console.log("base64Image", base64Image)
+    // Что было.
+    // Скачиваем изображение с сервера телеграмм и отправляем его на сервер search4faces.com для обнаружения лиц
+    const response = await axios.post('https://search4faces.com/api/json-rpc/v1', {
+        jsonrpc: '2.0',
+        method: 'detectFaces',
+        id: 'some-id',
+        params: {
+            image: base64Image
         }
-    )
-    // Обрабатываем ответ от API
-    console.log(response.data)
-    const faces = response.data.result.faces;
+    }, {
+        headers: {
+            'Content-Type': 'application/json',
+            'x-authorization-token': search4facesToken
+        }
+    });
+
+
+    const faces = response.result.faces;
     if (faces.length > 0) {
-        // Если на изображении обнаружены лица, отправляем запрос на API сайта search4faces.com для поиска похожих лиц в социальных сетях
-        const searchResponse = await axios.post('https://search4faces.com/api/json-rpc/v1', {
-            jsonrpc: '2.0',
-            method: 'searchFace',
-            id: 'some-id',
-            params: {
-                image: fileInfo.fileLink,
-                face: faces[0].base64,
-                maxResults: 10
-            }
-        }, {
+        const searchResponse = await axios({
+            method: 'post',
+            url: 'https://search4faces.com/api/json-rpc/v1',
+            data: {
+                jsonrpc: "2.0",
+                method: "searchFace",
+                id: 100,
+                params: {
+                    image: response.result.image,
+                    face: response.result.faces[0],
+                    source: "vk_wall",
+                    hidden: true,
+                    results: 10,
+                    lang: "ru"
+                }
+            },
             headers: {
                 'Content-Type': 'application/json',
                 'x-authorization-token': search4facesToken
             }
-        });
+
+        })
 
         // Обрабатываем ответ от API и отправляем пользователю ссылки на найденные профили в социальных сетях
         const profiles = searchResponse.data.result.profiles;
         if (profiles.length > 0) {
             let message = 'Найдены похожие лица в следующих социальных сетях:\n';
             profiles.forEach(profile => {
-                message += `- ${profile.url}\n`;
+                message += `- ${profile.profile}\n`;
             });
-            bot.sendMessage(chatId, message);
+            await bot.sendMessage(chatId, message);
         } else {
-            bot.sendMessage(chatId, 'Похожие лица не найдены.');
+            await bot.sendMessage(chatId, 'Похожие лица не найдены.');
         }
-    } else {
+    }
+    else {
         // Если на изображении не обнаружены лица, отправляем сообщение об ошибке
-        bot.sendMessage(chatId, 'На изображении не обнаружены лица.');
+        await bot.sendMessage(chatId, 'На изображении не обнаружены лица.');
     }
 });
